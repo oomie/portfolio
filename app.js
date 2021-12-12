6da7bd2b1e25005ad4480e7429db9a82f897c9d6
oomie <fma9304@nyu.edu>
2021-12-13 03:10:13 +0400

    fixed interactivity

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 
-var intro = document.getElementById("intro");
-var music = document.getElementById('music');
+var bio = document.getElementById("bio");
+var hats = document.getElementById('hats');
+var comix = document.getElementById('comix');
+var sound = document.getElementById('sound');
+var vid = document.getElementById('vid');
 
-intro.addEventListener('click',()=> {
-  intro.style.display = "none"
-  intro.play();
-})
-// music.addEventListener('click', () => {
-//   music.play();
+
+// intro.addEventListener('click',()=> {
+//   intro.style.display = "none"
+//   intro.play();
 // })
+bio.addEventListener('click',()=> {
+alert('Fun fact:\n I collect a lot of things.. some of my collections include beaded bracelets, plushies and Studio Ghibli puzzles! ');
+})
+
+hats.addEventListener('click',()=> {
+alert('Fun fact:\n We decided to make our film a commercial about bucket hats because Eva has an extensive collection of them!');
+})
+
+comix.addEventListener('click',()=> {
+alert('Fun fact:\nWe had a few options for the title of this comic, but we ultimately decided on City Tripping after spinning a wheel. The winning title was toomies suggestion!');
+})
+
+sound.addEventListener('click',()=> {
+alert('Fun fact:\nWhen toomie was recording the princess battle grunts, she looked up Genshin characters battle and attack lines for guidance!');
+})
+
+vid.addEventListener('click',()=> {
+  alert('Fun fact:\nDania suggested the murder theme. A little suspicious of her if you ask me...');
+})
